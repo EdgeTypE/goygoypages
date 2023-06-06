6c5d6da0d98a5686a98043faf5a44bd9e8785d0c
EdgeTypE <34396598+EdgeTypE@users.noreply.github.com>
2023-06-06 12:30:11 +0300

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -3,15 +3,15 @@ import type { SiteConfig } from '$lib/types/site'
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
-  lang: 'en-US',
+  title: 'Goygoy Engine',
+  subtitle: 'Oyun Haberleri',
+  lang: 'tr-TR',
   description: 'Powered by SvelteKit/Urara',
   author: {
-    avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
-    status: '🌸',
-    bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    avatar: '/assets/goygoyenginekare.png',
+    name: 'Goygoy Engine',
+    status: '👾',
+    bio: 'Oyun sektörü haberlerini sizlerle paylaşıyoruz.'
   },
   themeColor: '#3D4451'
 }
